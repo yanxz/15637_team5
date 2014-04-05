3cf463a902cc415fc799e8844539f539b92a12d4
Xuezhi <xuezhi@xuezhi-ThinkPad-X220.(none)>
2014-04-05 00:46:18 -0400

    test1

--- scene/RemiScene/scene_view.py
+++ scene/RemiScene/scene_view.py
@@ -24,19 +24,31 @@ import random
 #@login_required
 def home(request, scene_id):
     scene = Scene.objects.get(id = scene_id)
+"""
+    # check if this user could view this scene
+    user_scenes = PersonScene.objects.filter(user = request.user)
 
+    scene_ids = []
+    for personscene in user_scenes:
+        scene_ids.add(personscene.scene_id)
+    scenes = Scene.objects.filter(id__in=scene_ids)
+
+    if scene not in scenes:
+        return redirect(reverse('not_authenticated'))
+"""
 # get all PersonScene objects
     person_scenes = PersonScene.objects.filter(scene = scene)
     comments = Comment.objects.filter(person_scene__in = person_scenes)
     comments_count = {}
     for comment in comments:
-        if comments_count[comment.person_scene] in comments_count:
+        if comment.person_scene in comments_count.keys():
             comments_count[comment.person_scene] = comments_count[comment.person_scene]+1
     	else:
             comments_count[comment.person_scene] = 1
 
-    person_scenes.sort(key = lambda x : comments_count[x])
+    person_scenes = sorted(person_scenes, key = lambda x : comments_count.get(x, 0), reverse=True)
 
+    context = {}
     context['person_scenes'] = person_scenes
     context['scene'] = scene
     return render(request, "RemiScene/testSceneHome.html", context)
