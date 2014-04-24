6785ca177bb2755b2629f836ac73c0aabd3de886
Chongyu Ren <rchongyu@gmail.com>
2014-04-24 00:46:45 -0400

    rcy

--- scene/RemiScene/home_view.py
+++ scene/RemiScene/home_view.py
@@ -117,7 +117,8 @@ def search_people(request):
 @login_required
 def add_friend(request,userid):
     user = request.user
-    if len(Friends.objects.filter(user=user,friend_id=userid)) == 0: 
+    friendships = Friends.objects.filter(user=user,friend_id=userid)
+    if len(friendships) == 0: 
         friendship = Friends(user=request.user,friend_id=userid)
         friendship.save()
 
@@ -128,7 +129,11 @@ def add_friend(request,userid):
         new_Message.save()
         add_friend_result = 'You have send a message to '+ friend.first_name + " " + friend.last_name
     else:
-        add_friend_result = friend.first_name + " " + friend.last_name + 'is already your friend.'
+        friend = User.objects.get(id=userid)
+        if friendships[0].is_active:
+            add_friend_result = friend.first_name + " " + friend.last_name + ' is already your friend.'
+        else:
+            add_friend_result = friend.first_name + " " + friend.last_name + ' has not comfirm the request.'
 
     context = {'add_friend_result': add_friend_result}
     return render(request, "RemiScene/search_people.html", context)
